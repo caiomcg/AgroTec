9c6c99a7a53ee63ee2cbfaff37814ee966a739be
caiomcg <caiomcg@gmail.com>
2018-11-02 13:18:38 -0300

    Use portuguese by default and correctly validate the create 'user' fields

--- app/src/main/java/com/caiomcg/es/Controllers/NoAccountActivity.java
+++ app/src/main/java/com/caiomcg/es/Controllers/NoAccountActivity.java
@@ -1,6 +1,8 @@
 package com.caiomcg.es.Controllers;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
@@ -13,11 +15,14 @@ import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.caiomcg.es.C;
+import com.caiomcg.es.Models.User;
 import com.caiomcg.es.R;
 import com.caiomcg.es.UserFactory;
 import com.caiomcg.es.Utils.Requests;
+import com.caiomcg.es.Validator;
 
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -68,23 +73,84 @@ public class NoAccountActivity extends AppCompatActivity implements View.OnClick
                 break;
 
             case R.id.create_user_button:
-                if (firstName.getText().toString().isEmpty() ||
-                        lastName.getText().toString().isEmpty() ||
-                        userName.getText().toString().isEmpty() ||
-                        email.getText().toString().isEmpty() ||
-                        password.getText().toString().isEmpty() ||
-                        repassword.getText().toString().isEmpty()) {
+                Log.e(TAG, "Validating: " + email.getText() + " - " + Validator.isValidEmailId(email.getText().toString()));
+
+                if (!validateFields()) {
                     new AlertDialog.Builder(NoAccountActivity.this)
                             .setTitle("Cadastro inválido")
-                            .setMessage("Por favor, preencha todos os campos")
+                            .setMessage("Por favor, verifique se os campos são válidos")
                             .setPositiveButton("Ok", null)
                             .create()
                             .show();
                 } else {
                     //Todo: send to server
+                    User user = new User();
+                    user.firstName = firstName.getText().toString();
+                    user.lastName = lastName.getText().toString();
+                    user.userName = userName.getText().toString();
+                    user.email = email.getText().toString();
+                    user.password = password.getText().toString();
+                    user.phone = "Adicionamos?"; // TODO: DECIDE
+                    JSONObject jsonObject = null;
+
+                    try {
+                        jsonObject = user.toJsonObject();
+                        Log.e(TAG, "Object: " + jsonObject.toString(4));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                    Requests.getInstance().asJsonObject(Request.Method.POST, C.userURI().toString(),
+                            jsonObject, response -> {
+                                Log.e(TAG, "All good");
+                            }, error -> {
+                                Log.e(TAG, "An error occured: " + error.toString());
+                                error.printStackTrace();
+                            });
                     Log.d(TAG, "Sending to server");
                 }
                 break;
         }
+    }
+
+    private boolean validateFields() {
+        return (validateEmptyField(firstName) &&
+                validateEmptyField(lastName) &&
+                validateEmptyField(userName) &&
+                validateEmptyField(email) &&
+                validateEmptyField(password) &&
+                validateEmptyField(repassword)) &&
+                validateEmailField(email) &&
+                passwordsMatch(password, repassword);
+
+    }
+
+    private boolean validateEmptyField(EditText editText) {
+        if (!editText.getText().toString().isEmpty()) {
+            editText.getBackground().setColorFilter(Color.GREEN, PorterDuff.Mode.SRC_IN);
+            return true;
+        }
+        editText.getBackground().setColorFilter(Color.RED, PorterDuff.Mode.SRC_IN);
+        return false;
+    }
+
+    private boolean validateEmailField(EditText editText) {
+        if (Validator.isValidEmailId(editText.getText().toString())) {
+            editText.getBackground().setColorFilter(Color.GREEN, PorterDuff.Mode.SRC_IN);
+            return true;
+        }
+        editText.getBackground().setColorFilter(Color.RED, PorterDuff.Mode.SRC_IN);
+        return false;
+    }
+
+    private boolean passwordsMatch(EditText first, EditText second) {
+        if (first.getText().toString().equals(second.getText().toString())) {
+            first.getBackground().setColorFilter(Color.GREEN, PorterDuff.Mode.SRC_IN);
+            second.getBackground().setColorFilter(Color.GREEN, PorterDuff.Mode.SRC_IN);
+            return true;
+        }
+        first.getBackground().setColorFilter(Color.RED, PorterDuff.Mode.SRC_IN);
+        second.getBackground().setColorFilter(Color.RED, PorterDuff.Mode.SRC_IN);
+        return false;
     }
 }
