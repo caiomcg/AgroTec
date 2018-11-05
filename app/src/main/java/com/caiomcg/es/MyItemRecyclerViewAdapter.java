230317f291885e67aac79edb87329378c70ba982
thiagoluiznunes <thiagoluiz.dev@gmail.com>
2018-11-05 21:19:20 -0200

    change item layout

--- app/src/main/java/com/caiomcg/es/MyItemRecyclerViewAdapter.java
+++ app/src/main/java/com/caiomcg/es/MyItemRecyclerViewAdapter.java
@@ -1,9 +1,11 @@
 package com.caiomcg.es;
 
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.caiomcg.es.MataFragment.OnListFragmentInteractionListener;
@@ -37,6 +39,10 @@ public class MyItemRecyclerViewAdapter extends RecyclerView.Adapter<MyItemRecycl
         holder.mTitle.setText(mValues.get(position));
         holder.mDescription.setText(mValues.get(position));
 
+        holder.mUserName.setText(mValues.get(position));
+        holder.mUserDate.setText(mValues.get(position));
+        //holder.mUserImageProfile.setImageURI(Uri.parse("https://avatars3.githubusercontent.com/u/11435231?s=400&u=55b9178d434edb72fc13a8b2f1bf46b47d48326e&v=4"));
+
         holder.mView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -58,12 +64,18 @@ public class MyItemRecyclerViewAdapter extends RecyclerView.Adapter<MyItemRecycl
         public final View mView;
         public final TextView mTitle;
         public final TextView mDescription;
+        public final TextView mUserName;
+        public final TextView mUserDate;
+        public final ImageView mUserImageProfile;
 
         public ViewHolder(View view) {
             super(view);
             mView = view;
             mTitle = (TextView) view.findViewById(R.id.item_mata_title);
             mDescription = (TextView) view.findViewById(R.id.item_mata_description);
+            mUserName = (TextView) view.findViewById(R.id.item_mata_userName);
+            mUserDate = (TextView) view.findViewById(R.id.item_mata_date);
+            mUserImageProfile = (ImageView) view.findViewById(R.id.item_mata_userImageProfile);
         }
     }
 }
